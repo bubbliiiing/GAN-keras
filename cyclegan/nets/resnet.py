586a9f5ad9e8f36210eb0c74ac2c30a07522ecf2
JiaQi Xu <47347516+bubbliiiing@users.noreply.github.com>
2020-01-23 12:26:31 +0800

    Update resnet.py

--- cyclegan/nets/resnet.py
+++ cyclegan/nets/resnet.py
@@ -19,7 +19,7 @@ def identity_block(input_tensor, kernel_size, filter_num, block):
 
     conv_name_base = 'res' + block + '_branch'
     in_name_base = 'in' + block + '_branch'
-    # 1x1压缩
+
     x = ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING)(input_tensor)
     x = Conv2D(filter_num, (3, 3) , data_format=IMAGE_ORDERING , name=conv_name_base + '2a')(x)
     x = InstanceNormalization(axis=3,name=in_name_base + '2a')(x)
@@ -36,37 +36,42 @@ def identity_block(input_tensor, kernel_size, filter_num, block):
 
 def get_resnet(input_height, input_width, channel):
     img_input = Input(shape=(input_height,input_width , 3 ))
-
+    # 128,128,3 -> 128,128,64
     x = ZeroPadding2D((3, 3), data_format=IMAGE_ORDERING)(img_input)
     x = Conv2D(64, (7, 7), data_format=IMAGE_ORDERING)(x)
     x = InstanceNormalization(axis=3)(x)
     x = Activation('relu')(x)
 
+    # 128,128,64 -> 64,64,128
     x = ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING)(x)
     x = Conv2D(128, (3, 3), data_format=IMAGE_ORDERING, strides=2)(x)
     x = InstanceNormalization(axis=3)(x)
     x = Activation('relu')(x)
 
+    # 64,64,128 -> 32,32,256
     x = ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING)(x)
     x = Conv2D(256, (3, 3), data_format=IMAGE_ORDERING, strides=2)(x)
     x = InstanceNormalization(axis=3)(x)
     x = Activation('relu')(x)
 
-    for i in range(6):
+    for i in range(9):
         x = identity_block(x, 3, 256, block=str(i))
-
+    
+    # 32,32,256 -> 64,64,128
     x = ( UpSampling2D( (2,2), data_format=IMAGE_ORDERING))(x)
     x = ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING)(x)
     x = Conv2D(128, (3, 3), data_format=IMAGE_ORDERING)(x)
     x = InstanceNormalization(axis=3)(x)
     x = Activation('relu')(x)
     
+    # 64,64,128 -> 128,128,64
     x = ( UpSampling2D( (2,2), data_format=IMAGE_ORDERING))(x)
     x = ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING)(x)
     x = Conv2D(64, (3, 3), data_format=IMAGE_ORDERING)(x)
     x = InstanceNormalization(axis=3)(x)
     x = Activation('relu')(x)    
 
+    # 128,128,64 -> 128,128,3
     x = ZeroPadding2D((3, 3), data_format=IMAGE_ORDERING)(x)
     x = Conv2D(channel, (7, 7), data_format=IMAGE_ORDERING)(x)
     x = Activation('tanh')(x)  
